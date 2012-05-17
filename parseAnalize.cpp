10a7a53b077d6ff9a08b915c6085ec8c5610050b
Remo Storni <remo.storni@gmail.com>
2012-05-17 09:51:14 +0900

    Saved version of parseAnalize

--- parseAnalize.cpp
+++ parseAnalize.cpp
@@ -2,7 +2,7 @@
 #include <fstream>
 #include <string>
 using namespace std;
-
+/*
 int main(){
   ifstream inputFile;
   ofstream outputFile;
@@ -20,4 +20,32 @@ int main(){
     outputFile.close();
   }
   return 0;
+}
+
+int main(){
+  double
+  ifstream inputFile;
+  ofstream outputFile;
+  inputFile.open("aapl.csv",ios::in);
+  string line;
+  if(inputFile.is_open()){
+    getline(inputfile,line);
+    while(getline(inputfile,line)){
+      stringstream lineStream(line);
+      string cell;
+
+    }
+
+
+  }
+
+
+}
+
+*/
+
+int main(){
+  char** strArray = new char[7];
+  
+
 }
